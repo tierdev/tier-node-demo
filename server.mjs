91a3832d7c15c70e88f28da10ea75edd463e5f85
isaacs <i@izs.me>
2022-10-27 21:33:00 -0700

    fix bugs, use mjs

--- server.mjs
+++ server.mjs
@@ -1,16 +1,20 @@
-const express = require('express')
-const { resolve } = require('path')
+import express from 'express'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const app = express()
 const port = +process.env.PORT || 80
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(require('./lib/routes.js'))
+import routes from './lib/routes.mjs'
+app.use(routes)
 
 app.use(express.static(resolve(__dirname, 'lib/static')))
 app.set('view engine', 'ejs')
